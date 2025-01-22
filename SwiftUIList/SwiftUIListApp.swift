49668672bab251997f36397435bd6ddf359e21f3
poga07 <chris@168money.com.tw>
2025-01-22 09:41:08 +0800

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by 黃柏嘉 on 2025/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
